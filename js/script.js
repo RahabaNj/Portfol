b33027d85cd95554d2128be1f5daeb18f5bf4470
AVOTRINIAINA Njakatiana <154663172+RahabaNj@users.noreply.github.com>
2024-01-31 10:20:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -71,3 +71,9 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+    if (x.matches) { // If media query matches
+      document.body.style.backgroundColor = "yellow";
+    } else {
+      document.body.style.backgroundColor = "pink";
+    }
